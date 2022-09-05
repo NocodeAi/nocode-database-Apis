cf5159762550c2b71a2feaae00437c5212194f9c
syedfakher27 <sfakher27@gmail.com>
2022-09-05 11:44:10 +0500

    controller updated

--- controllers/post.js
+++ controllers/post.js
@@ -7,7 +7,7 @@ const { Sequelize } = require('sequelize')
 
 const validate_connection = async (req,res) => 
 {
-   const {username,password,host,dialect,port,database} = req?.body
+   const {username,password,host,dialect,port,database,project_id} = req?.body
 
    console.log(username,password,host,dialect,port)
    const config = {
@@ -24,6 +24,11 @@ const validate_connection = async (req,res) =>
     await db.authenticate()
     console.log('database is connected successfully...')
     db.sync()
+    let id = uuidv4()
+    let sql = `INSERT INTO public.databases(
+        id, project_id, user_name, password, database_name, host, port, type, is_deleted, created_at, updated_at)
+        VALUES ('${id}', '${project_id}', '${username}', '${password}', '${database}', '${host}', ${port}, '${dialect}', false, NOW(), NOW())`
+    await runQuery(nocodeAi,sql)
     let obj = {
         success:true,
         message:'database is connected successfully'
